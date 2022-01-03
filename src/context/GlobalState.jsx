188fa0054ea3ba5b94bfb76d3a2a3cdc7ecaabf3
Andrea Guffi <andreaguffi92@gmail.com>
2022-01-03 20:40:37 +0100

    ok

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -14,6 +14,10 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = props => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    useEffect(() => {
+        localStorage.setItem('watchlist', JSON.stringify(state.watchlist))
+    }, [state])
+
     // actions
     const addMovieToWatchlist = movie => {
         dispatch({ type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie });
