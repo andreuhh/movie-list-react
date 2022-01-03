556b6c357f3cf76b2793f3622e531b53deefd287
Andrea Guffi <andreaguffi92@gmail.com>
2022-01-03 20:51:05 +0100

    ok

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -3,8 +3,12 @@ import AppReducer from './AppReducer';
 
 // initial state
 const initialState = {
-    watchlist: [],
-    watched: [],
+    watchlist: localStorage.getItem("watchlist")
+        ? JSON.parse(localStorage.getItem("watchlist"))
+        : [],
+    watched: localStorage.getItem("watched")
+        ? JSON.parse(localStorage.getItem("watched"))
+        : [],
 };
 
 // create context
@@ -15,7 +19,8 @@ export const GlobalProvider = props => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     useEffect(() => {
-        localStorage.setItem('watchlist', JSON.stringify(state.watchlist))
+        localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+        localStorage.setItem('watched', JSON.stringify(state.watched));
     }, [state])
 
     // actions
